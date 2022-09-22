6a34d2bdc15e9f56a47d99bebf1d88bced144859
Aldo Tobing <aldo_tobing@hotmail.com>
2022-09-22 14:24:25 +0700

    view model

--- usecase/viewmodel/aws_vm.go
+++ usecase/viewmodel/aws_vm.go
@@ -4,6 +4,6 @@ package viewmodel
 type AwsVM struct {
 	FileName string `json:"file_name"`
 	FilePath string `json:"file_path"`
-	FileSize int64  `json:"size"`
-	FileType string `json:"type"`
+	FileSize int64  `json:"file_size"`
+	FileType string `json:"file_type"`
 }
