68e6a9af1933272a93c61ac0a334e2f71219220b
dennistjahyadi <dennis.tgt@gmail.com>
2022-12-14 14:36:02 +0700

    videopromote

---
+++ db/repository/models/VideoPromote.go
@@ -0,0 +1,49 @@
+package models
+
+// VideoPromote ...
+type VideoPromote struct {
+	ID          *string `json:"id"`
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+	StartDate   *string `json:"start_date"`
+	EndDate     *string `json:"end_date"`
+	Active      *string `json:"active"`
+	Url         *string `json:"url"`
+}
+
+// VideoPromoteParameter ...
+type VideoPromoteParameter struct {
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	StartDate string `json:"start_date"`
+	EndDate   string `json:"end_date"`
+	UserId    string `json:"user_id"`
+	Search    string `json:"search"`
+	Page      int    `json:"page"`
+	Offset    int    `json:"offset"`
+	Limit     int    `json:"limit"`
+	By        string `json:"by"`
+	Sort      string `json:"sort"`
+}
+
+var (
+	// VideoPromoteOrderBy ...
+	VideoPromoteOrderBy = []string{"def.id", "def.title"}
+	// VideoPromoteOrderByrByString ...
+	VideoPromoteOrderByrByString = []string{
+		"def.title",
+	}
+
+	// VideoPromoteSelectStatement ...
+	VideoPromoteSelectStatement = `
+	SELECT DEF.ID AS ID,
+		DEF.TITLE AS TITLE,
+		DEF.DESCRIPTION AS DESCRIPTION,
+		DEF.START_DATE AS VideoPromote_START_DATE,
+		DEF.END_DATE AS VideoPromote_END_DATE,
+		DEF.URL AS URL 
+	FROM video_promote DEF`
+
+	// VideoPromoteWhereStatement ...
+	VideoPromoteWhereStatement = ` where def.id is not null and def.active = 1 `
+)
---
+++ server/handlers/video_promote_handler.go
@@ -0,0 +1,94 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"nextbasis-service-v-0.1/db/repository/models"
+	"nextbasis-service-v-0.1/pkg/str"
+	"nextbasis-service-v-0.1/server/requests"
+	"nextbasis-service-v-0.1/usecase"
+)
+
+// VideoPromoteHandler ...
+type VideoPromoteHandler struct {
+	Handler
+}
+
+// SelectAll ...
+func (h *VideoPromoteHandler) SelectAll(ctx *fiber.Ctx) error {
+	c := ctx.Locals("ctx").(context.Context)
+
+	parameter := models.VideoPromoteParameter{
+		Title:     ctx.Query("title"),
+		StartDate: ctx.Query("start_date"),
+		EndDate:   ctx.Query("end_date"),
+		UserId:    ctx.Query("user_id"),
+		Search:    ctx.Query("search"),
+		By:        ctx.Query("by"),
+		Sort:      ctx.Query("sort"),
+	}
+	uc := usecase.VideoPromoteUC{ContractUC: h.ContractUC}
+	res, err := uc.SelectAll(c, parameter)
+
+	type InvoceHeaderObjcet struct {
+		ListObjcet []models.VideoPromote `json:"list_VideoPromote"`
+	}
+
+	ObjcetData := new(InvoceHeaderObjcet)
+
+	if res != nil {
+		ObjcetData.ListObjcet = res
+	}
+
+	return h.SendResponse(ctx, ObjcetData, nil, err, 0)
+}
+
+// FindAll ...
+func (h *VideoPromoteHandler) FindAll(ctx *fiber.Ctx) error {
+	c := ctx.Locals("ctx").(context.Context)
+
+	parameter := models.VideoPromoteParameter{
+		Title:  ctx.Query("title"),
+		Search: ctx.Query("search"),
+		Page:   str.StringToInt(ctx.Query("page")),
+		Limit:  str.StringToInt(ctx.Query("limit")),
+		By:     ctx.Query("by"),
+		Sort:   ctx.Query("sort"),
+	}
+	uc := usecase.VideoPromoteUC{ContractUC: h.ContractUC}
+	res, meta, err := uc.FindAll(c, parameter)
+
+	type InvoceHeaderObjcet struct {
+		ListObjcet []models.VideoPromote `json:"list_VideoPromote"`
+	}
+
+	ObjcetData := new(InvoceHeaderObjcet)
+
+	if res != nil {
+		ObjcetData.ListObjcet = res
+	}
+
+	return h.SendResponse(ctx, ObjcetData, meta, err, 0)
+}
+
+// Add ...
+func (h *VideoPromoteHandler) Add(ctx *fiber.Ctx) error {
+	c := ctx.Locals("ctx").(context.Context)
+
+	input := new(requests.VideoPromoteRequest)
+	if err := ctx.BodyParser(input); err != nil {
+		return h.SendResponse(ctx, nil, nil, err, http.StatusBadRequest)
+	}
+	if err := h.Validator.Struct(input); err != nil {
+		errMessage := h.ExtractErrorValidationMessages(err.(validator.ValidationErrors))
+		return h.SendResponse(ctx, nil, nil, errMessage, http.StatusBadRequest)
+	}
+
+	uc := usecase.VideoPromoteUC{ContractUC: h.ContractUC}
+	res, err := uc.Add(c, input)
+
+	return h.SendResponse(ctx, res, nil, err, 0)
+}
