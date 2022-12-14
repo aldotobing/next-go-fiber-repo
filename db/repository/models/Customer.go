e4dda675f2c4a7e522be093cce143572775e0ce9
Aldo Tobing <aldo_tobing@hotmail.com>
2022-12-14 11:11:58 +0700

    customer_update

--- db/repository/models/Customer.go
+++ db/repository/models/Customer.go
@@ -7,12 +7,15 @@ type Customer struct {
 	CustomerName            *string `json:"customer_name"`
 	CustomerProfilePicture  *string `json:"customer_profile_picture"`
 	CustomerActiveStatus    *string `json:"customer_active_status"`
+	CustomerBirthDate       *string `json:"customer_birthdate"`
+	CustomerReligion        *string `json:"customer_religion"`
 	CustomerLatitude        *string `json:"customer_latitude"`
 	CustomerLongitude       *string `json:"customer_longitude"`
 	CustomerBranchCode      *string `json:"customer_branch_code"`
 	CustomerBranchName      *string `json:"customer_branch_name"`
 	CustomerRegionCode      *string `json:"customer_region_code"`
 	CustomerRegionName      *string `json:"customer_region_name"`
+	CustomerRegionGroup     *string `json:"customer_region_group"`
 	CustomerEmail           *string `json:"customer_email"`
 	CustomerCpName          *string `json:"customer_cp_name"`
 	CustomerAddress         *string `json:"customer_address"`
@@ -72,6 +75,8 @@ var (
 		C.CUSTOMER_ADDRESS AS CUST_ADDRESS,
 		C.CUSTOMER_PROFILE_PICTURE AS CUST_PROFILE_PICTURE,
 		C.CUSTOMER_EMAIL AS CUST_EMAIL,
+		C.CUSTOMER_BIRTHDATE AS BIRTHDATE,
+		C.CUSTOMER_RELIGION AS RELIGION,
 		CASE C.ACTIVE
 				WHEN 1 THEN 'Active'
 				WHEN 0 THEN 'Inactive'
@@ -82,6 +87,7 @@ var (
 		B._NAME AS BRANCH_NAME,
 		REG.CODE AS REGION_CODE,
 		REG._NAME AS REGION_NAME,
+		REG.GROUP_NAME AS REGION_GROUP,
 		PRV.ID AS CUST_PROVINCE_ID,
 		PRV._NAME AS CUST_PROVINCE_NAME,
 		CTY.ID AS CUST_CITY_ID,
@@ -96,7 +102,7 @@ var (
 		C.SALES_CYCLE CUST_SALES_CYCLE,
 		C.CUSTOMER_TYPE_ID AS CUST_TYPE_ID,
 		CT._NAME CUST_TYPE_NAME,
-		C.CUSTOMER_PHONE AS CUSTOMER_PHONE,
+		'+62' || regexp_replace(SUBSTRING (customer_phone, 2, length(customer_phone)),'[^\w]+','','g' )  AS CUSTOMER_PHONE,
 		(SELECT SUM(SIH.TRANSACTION_POINT) FROM SALES_INVOICE_HEADER SIH WHERE SIH.CUST_BILL_TO_ID = C.ID) AS CUSTOMER_POINT,
 		CG.GIFT_NAME AS CUST_GIFT_NAME,
 		LOY.LOYALTY_NAME AS CUST_LOYALTY_NAME
