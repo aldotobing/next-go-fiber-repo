ddc526cfc9755b6ceefef07b37b272214c340cba
Billy <bilpang.fcl@gmail.com>
2023-03-08 15:39:51 +0700

    item details: find by id V2

--- usecase/item_details_uc.go
+++ usecase/item_details_uc.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"strconv"
 
 	"nextbasis-service-v-0.1/db/repository"
 	"nextbasis-service-v-0.1/db/repository/models"
@@ -66,7 +67,61 @@ func (uc ItemDetailsUC) FindByID(c context.Context, parameter models.ItemDetails
 		logruslogger.Log(logruslogger.WarnLevel, err.Error(), functioncaller.PrintFuncName(), "query", c.Value("requestid"))
 		return res, err
 	}
-	uc.BuildBody(&res)
+
+	return res, err
+}
+
+// FindByIDV2 ...
+func (uc ItemDetailsUC) FindByIDV2(c context.Context, parameter models.ItemDetailsParameter) (res viewmodel.ItemDetailsVM, err error) {
+	repo := repository.NewItemDetailsRepository(uc.DB)
+	data, err := repo.FindByIDV2(c, parameter)
+	if err != nil {
+		logruslogger.Log(logruslogger.WarnLevel, err.Error(), functioncaller.PrintFuncName(), "query", c.Value("requestid"))
+		return
+	}
+
+	// Find Lowest Price and lowest conversion
+	var lowestPrice, lowestConversion float64
+	for _, datum := range data {
+		price, _ := strconv.ParseFloat(*datum.ItemDetailsPrice, 64)
+		conversion, _ := strconv.ParseFloat(*datum.UomLineConversion, 64)
+		if price < lowestPrice || lowestPrice == 0 {
+			lowestPrice = price
+			lowestConversion = conversion
+		}
+	}
+
+	data, err = repo.FindContainByItemIDV2(c, parameter)
+	if err != nil {
+		logruslogger.Log(logruslogger.WarnLevel, err.Error(), functioncaller.PrintFuncName(), "query", c.Value("requestid"))
+		return 
+	}
+
+	basePrice := lowestPrice / lowestConversion
+	var uoms []viewmodel.Uom
+	for _, datum := range data {
+		conversion, _ := strconv.ParseFloat(*datum.UomLineConversion, 64)
+		price := strconv.FormatFloat(basePrice*conversion, 'f', 2, 64)
+
+		uoms = append(uoms, viewmodel.Uom{
+			ID:               datum.UomID,
+			Name:             datum.UomName,
+			Conversion:       datum.UomLineConversion,
+			ItemDetailsPrice: &price,
+		})
+	}
+
+	res = viewmodel.ItemDetailsVM{
+		ID:                      data[0].ID,
+		Code:                    data[0].Code,
+		Name:                    data[0].Name,
+		Description:             data[0].Description,
+		ItemDetailsCategoryId:   data[0].ItemDetailsCategoryId,
+		ItemDetailsCategoryName: data[0].ItemDetailsCategoryName,
+		ItemDetailsPicture:      data[0].ItemDetailsPicture,
+		Uom:                     uoms,
+		PriceListVersionId:      &parameter.PriceListVersionId,
+	}
 
 	return res, err
 }
