d0bd0b98261bd38919af30a05dd5912f6a41bd2f
Billy <bilpang.fcl@gmail.com>
2023-03-09 14:41:32 +0700

    Promo Item Line: Bulk add

--- server/requests/web_promo_item_line_request.go
+++ server/requests/web_promo_item_line_request.go
@@ -2,8 +2,13 @@ package requests
 
 // WebPromoItemLineRequest ...
 type WebPromoItemLineRequest struct {
-	PromoLineID string `json:"promo_line_id"`
-	ItemID      string `json:"item_id"`
-	UomID       string `json:"uom_id"`
-	Qty         string `json:"qty"`
+	PromoLineID string                               `json:"promo_line_id" validate:"required"`
+	Items       []WebPromoItemLineItemDetailsRequest `json:"items"`
+}
+
+// WebPromoItemLineItemDetailsRequest ...
+type WebPromoItemLineItemDetailsRequest struct {
+	ItemID string `json:"item_id" validate:"required"`
+	UomID  string `json:"uom_id" validate:"required"`
+	Qty    string `json:"qty" validate:"required"`
 }
