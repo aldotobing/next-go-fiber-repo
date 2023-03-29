09b33c3decee3766924d80e7fdc69bb1324ba249
Billy <bilpang.fcl@gmail.com>
2023-03-29 15:42:19 +0700

    web customer apppend with url on logic

--- usecase/web_customer_uc.go
+++ usecase/web_customer_uc.go
@@ -39,7 +39,10 @@ func (uc WebCustomerUC) BuildBody(data *models.WebCustomer, res *viewmodel.Custo
 	res.ID = data.ID
 	res.Code = data.Code
 	res.CustomerName = data.CustomerName
-	res.CustomerProfilePicture = data.CustomerProfilePicture
+
+	profilePictureURL := models.CustomerImagePath + *data.CustomerProfilePicture
+	res.CustomerProfilePicture = &profilePictureURL
+
 	res.CustomerActiveStatus = data.CustomerActiveStatus
 	res.CustomerBirthDate = data.CustomerBirthDate
 	res.CustomerReligion = data.CustomerReligion
@@ -82,7 +85,10 @@ func (uc WebCustomerUC) BuildBody(data *models.WebCustomer, res *viewmodel.Custo
 	res.CustomerBranchID = data.CustomerBranchID
 	res.CustomerSalesmanID = data.CustomerSalesmanID
 	res.CustomerNik = data.CustomerNik
-	res.CustomerPhotoKtp = data.CustomerPhotoKtp
+
+	photoktpURL := models.CustomerImagePath + *data.CustomerPhotoKtp
+	res.CustomerPhotoKtp = &photoktpURL
+
 	res.CustomerLevelID = data.CustomerLevelID
 	res.CustomerLevel = data.CustomerLevel
 	res.CustomerUserID = data.CustomerUserID
