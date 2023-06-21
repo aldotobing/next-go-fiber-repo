83d592da7961a766b967341845be9785de0edf3c
MYOADHIEY <moch.yuliadipurwanto@gmail.com>
2023-06-22 03:58:59 +0700

    tescron-synccustomer

--- helper/cron_helper.go
+++ helper/cron_helper.go
@@ -76,7 +76,7 @@ func SetCronJobs() {
 
 	})
 
-	c.AddFunc("CRON_TZ=Asia/Jakarta 0/5 * * * *", func() {
+	c.AddFunc("CRON_TZ=Asia/Jakarta 0/6 * * * *", func() {
 		url := envConfig["APP_BASE_URL"] + "/v1/api/sync/master/customer"
 		client := &http.Client{}
 		req, _ := http.NewRequest("GET", url, nil)
