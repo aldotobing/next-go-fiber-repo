8cf9ee601be5d713e1c47ada1772c12913cc280b
MYOADHIEY <moch.yuliadipurwanto@gmail.com>
2023-08-01 09:13:43 +0700

    tes

--- helper/cron_helper.go
+++ helper/cron_helper.go
@@ -25,18 +25,18 @@ func SetCronJobs() {
 
 	})
 
-	c.AddFunc("CRON_TZ=Asia/Jakarta 0/14 * * * *", func() {
-		url := envConfig["APP_BASE_URL"] + "/v1/api/sync/transaction/invoicedata"
-		client := &http.Client{}
-		req, _ := http.NewRequest("GET", url, nil)
-		req.Header.Set("Authorization", "Basic Og==")
-		res, _ := client.Do(req)
+	// c.AddFunc("CRON_TZ=Asia/Jakarta 0/14 * * * *", func() {
+	// 	url := envConfig["APP_BASE_URL"] + "/v1/api/sync/transaction/invoicedata"
+	// 	client := &http.Client{}
+	// 	req, _ := http.NewRequest("GET", url, nil)
+	// 	req.Header.Set("Authorization", "Basic Og==")
+	// 	res, _ := client.Do(req)
 
-		if res != nil {
-			// fmt.Println("error")
-		}
+	// 	if res != nil {
+	// 		// fmt.Println("error")
+	// 	}
 
-	})
+	// })
 
 	c.AddFunc("CRON_TZ=Asia/Jakarta 0/2 * * * *", func() {
 		url := envConfig["APP_BASE_URL"] + "/v1/api/sync/transaction/voidedrequest"
@@ -77,32 +77,32 @@ func SetCronJobs() {
 
 	})
 
-	c.AddFunc("CRON_TZ=Asia/Jakarta 0/6 * * * *", func() {
-		fmt.Println("execute scheduller customer data")
-		url := envConfig["APP_BASE_URL"] + "/v1/api/sync/master/customer"
-		client := &http.Client{}
-		req, _ := http.NewRequest("GET", url, nil)
-		req.Header.Set("Authorization", "Basic Og==")
-		res, _ := client.Do(req)
+	// c.AddFunc("CRON_TZ=Asia/Jakarta 0/6 * * * *", func() {
+	// 	fmt.Println("execute scheduller customer data")
+	// 	url := envConfig["APP_BASE_URL"] + "/v1/api/sync/master/customer"
+	// 	client := &http.Client{}
+	// 	req, _ := http.NewRequest("GET", url, nil)
+	// 	req.Header.Set("Authorization", "Basic Og==")
+	// 	res, _ := client.Do(req)
 
-		if res != nil {
-			// fmt.Println("error")
-		}
+	// 	if res != nil {
+	// 		// fmt.Println("error")
+	// 	}
 
-	})
+	// })
 
-	c.AddFunc("CRON_TZ=Asia/Jakarta 0/5 * * * *", func() {
-		url := envConfig["APP_BASE_URL"] + "/v1/api/sync/master/salesman"
-		client := &http.Client{}
-		req, _ := http.NewRequest("GET", url, nil)
-		req.Header.Set("Authorization", "Basic Og==")
-		res, _ := client.Do(req)
+	// c.AddFunc("CRON_TZ=Asia/Jakarta 0/5 * * * *", func() {
+	// 	url := envConfig["APP_BASE_URL"] + "/v1/api/sync/master/salesman"
+	// 	client := &http.Client{}
+	// 	req, _ := http.NewRequest("GET", url, nil)
+	// 	req.Header.Set("Authorization", "Basic Og==")
+	// 	res, _ := client.Do(req)
 
-		if res != nil {
-			// fmt.Println("error")
-		}
+	// 	if res != nil {
+	// 		// fmt.Println("error")
+	// 	}
 
-	})
+	// })
 
 	c.AddFunc("CRON_TZ=Asia/Jakarta 0/10 * * * *", func() {
 		url := envConfig["APP_BASE_URL"] + "/v1/api/sync/master/item"
