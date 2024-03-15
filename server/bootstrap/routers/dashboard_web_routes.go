daf169e2a89afaa6a960de696746e291b433974e
MYOADHIEY <moch.yuliadipurwanto@gmail.com>
2024-03-15 15:50:17 +0700

    all-shutdown

--- server/bootstrap/routers/dashboard_web_routes.go
+++ server/bootstrap/routers/dashboard_web_routes.go
@@ -1,11 +1,7 @@
 package routers
 
 import (
-	"time"
-
-	"nextbasis-service-v-0.1/pkg/str"
 	"nextbasis-service-v-0.1/server/handlers"
-	"nextbasis-service-v-0.1/server/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -18,15 +14,15 @@ type DashboardWebRoutes struct {
 
 // RegisterRoute register DashboardWeb routes
 func (route DashboardWebRoutes) RegisterRoute() {
-	handler := handlers.DashboardWebHandler{Handler: route.Handler}
-	// jwtMiddleware := middlewares.JwtMiddleware{ContractUC: handler.ContractUC}
+	// handler := handlers.DashboardWebHandler{Handler: route.Handler}
+	// // jwtMiddleware := middlewares.JwtMiddleware{ContractUC: handler.ContractUC}
 
-	r := route.RouterGroup.Group("/api/web/dashboard")
-	// r.Use(jwtMiddleware.VerifyUser)
-	r.Use(middlewares.SavingContextValue(time.Duration(str.StringToInt(route.Handler.ContractUC.EnvConfig["APP_TIMEOUT"])) * time.Second))
-	r.Get("/", handler.GetData)
-	r.Get("/group", handler.GetDataByGroupID)
-	r.Get("/detail", handler.GetRegionDetailData)
+	// r := route.RouterGroup.Group("/api/web/dashboard")
+	// // r.Use(jwtMiddleware.VerifyUser)
+	// r.Use(middlewares.SavingContextValue(time.Duration(str.StringToInt(route.Handler.ContractUC.EnvConfig["APP_TIMEOUT"])) * time.Second))
+	// r.Get("/", handler.GetData)
+	// r.Get("/group", handler.GetDataByGroupID)
+	// r.Get("/detail", handler.GetRegionDetailData)
 	// r.Get("/detail/total/registered-user", handler.GetUserByRegionDetailData)
 	// r.Get("/branch", handler.GetBranchCustomerData)
 	// r.Get("/branch/select", handler.GetAllBranchCustomerData)
