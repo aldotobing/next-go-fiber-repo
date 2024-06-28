7beb0b8b3d6eceedda5fdebb284ec3772e688330
Aldo Tobing <aldo_tobing@hotmail.com>
2024-06-28 20:33:27 +0700

    add web customer target and visit exception

--- helper/customer_target_helper.go
+++ helper/customer_target_helper.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 
 	"nextbasis-service-v-0.1/db/repository/models"
@@ -12,11 +12,18 @@ import (
 
 func FetchClientDataTarget(params models.CustomerTargetSemesterParameter) (res interface{}) {
 	jsonReq, err := json.Marshal(params)
+	if err != nil {
+		fmt.Println("client err")
+		fmt.Print(err.Error())
+		return "fail fetch client_data_target : mysmagon"
+	}
+
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "http://nextbasis.id:8080/mysmagonsrv/rest/customertarget/data/1", bytes.NewBuffer(jsonReq))
 	if err != nil {
 		fmt.Println("client err")
 		fmt.Print(err.Error())
+		return "fail fetch client_data_target : mysmagon"
 	}
 
 	req.Header.Add("Accept", "application/json")
@@ -25,37 +32,88 @@ func FetchClientDataTarget(params models.CustomerTargetSemesterParameter) (res i
 
 	resp, err := client.Do(req)
 	if err != nil {
-
 		fmt.Print(err.Error())
+		return "fail fetch client_data_target : mysmagon"
 	}
 	defer resp.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Print(err.Error())
+		return "fail fetch client_data_target : mysmagon"
 	}
 
-	type resutlData struct {
+	type resultData struct {
 		SemesterTarget string `json:"semester_target"`
 		CurrentTarget  string `json:"current_target"`
 		QuartalTarget  string `json:"quartal_target"`
 		AnualTarget    string `json:"anual_target"`
 	}
 
-	objectData := new(resutlData)
+	objectData := new(resultData)
 
-	// var responseObject http.Response
-	json.Unmarshal(bodyBytes, &objectData)
+	err = json.Unmarshal(bodyBytes, &objectData)
+	if err != nil {
+		fmt.Print(err.Error())
+		return "fail fetch client_data_target : mysmagon"
+	}
 
 	return objectData
 }
 
+// func FetchClientDataTarget(params models.CustomerTargetSemesterParameter) (res interface{}) {
+// 	jsonReq, err := json.Marshal(params)
+// 	client := &http.Client{}
+// 	req, err := http.NewRequest("GET", "http://nextbasis.id:8080/mysmagonsrv/rest/customertarget/data/1", bytes.NewBuffer(jsonReq))
+// 	if err != nil {
+// 		fmt.Println("client err")
+// 		fmt.Print(err.Error())
+// 	}
+
+// 	req.Header.Add("Accept", "application/json")
+// 	req.Header.Add("Content-Type", "application/json")
+// 	req.Header.Add("Authorization", "Bearer C2A5CE6A2292E7745CE5A3F7E68A9")
+
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+
+// 		fmt.Print(err.Error())
+// 	}
+// 	defer resp.Body.Close()
+// 	bodyBytes, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Print(err.Error())
+// 	}
+
+// 	type resutlData struct {
+// 		SemesterTarget string `json:"semester_target"`
+// 		CurrentTarget  string `json:"current_target"`
+// 		QuartalTarget  string `json:"quartal_target"`
+// 		AnualTarget    string `json:"anual_target"`
+// 	}
+
+// 	objectData := new(resutlData)
+
+// 	// var responseObject http.Response
+// 	json.Unmarshal(bodyBytes, &objectData)
+
+// 	return objectData
+// }
+
 func FetchVisitDay(params models.CustomerParameter) interface{} {
 	jsonReq, err := json.Marshal(params)
+	if err != nil {
+		fmt.Println("client err")
+		fmt.Print(err.Error())
+		return "fail fetch customer_visit_day : mysmagon"
+	}
+
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "http://nextbasis.id:8080/mysmagonsrv/rest/customer/visitday/1", bytes.NewBuffer(jsonReq))
 	if err != nil {
 		fmt.Println("client err")
 		fmt.Print(err.Error())
+		return "fail fetch customer_visit_day : mysmagon"
 	}
 
 	req.Header.Add("Accept", "application/json")
@@ -64,24 +122,66 @@ func FetchVisitDay(params models.CustomerParameter) interface{} {
 
 	resp, err := client.Do(req)
 	if err != nil {
-
 		fmt.Print(err.Error())
+		return "fail fetch customer_visit_day : mysmagon"
 	}
 	defer resp.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Print(err.Error())
+		return "fail fetch customer_visit_day : mysmagon"
 	}
 
-	type resutlData struct {
+	type resultData struct {
 		VisitDay  string `json:"visit_day"`
 		VisitWeek string `json:"visit_week"`
 	}
 
-	ObjectData := new(resutlData)
+	objectData := new(resultData)
 
-	// var responseObject http.Response
-	json.Unmarshal(bodyBytes, &ObjectData)
+	err = json.Unmarshal(bodyBytes, &objectData)
+	if err != nil {
+		fmt.Print(err.Error())
+		return "fail fetch customer_visit_day : mysmagon"
+	}
 
-	return ObjectData
+	return objectData
 }
+
+// func FetchVisitDay(params models.CustomerParameter) interface{} {
+// 	jsonReq, err := json.Marshal(params)
+// 	client := &http.Client{}
+// 	req, err := http.NewRequest("GET", "http://nextbasis.id:8080/mysmagonsrv/rest/customer/visitday/1", bytes.NewBuffer(jsonReq))
+// 	if err != nil {
+// 		fmt.Println("client err")
+// 		fmt.Print(err.Error())
+// 	}
+
+// 	req.Header.Add("Accept", "application/json")
+// 	req.Header.Add("Content-Type", "application/json")
+// 	req.Header.Add("Authorization", "Bearer C2A5CE6A2292E7745CE5A3F7E68A9")
+
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+
+// 		fmt.Print(err.Error())
+// 	}
+// 	defer resp.Body.Close()
+// 	bodyBytes, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Print(err.Error())
+// 	}
+
+// 	type resutlData struct {
+// 		VisitDay  string `json:"visit_day"`
+// 		VisitWeek string `json:"visit_week"`
+// 	}
+
+// 	ObjectData := new(resutlData)
+
+// 	// var responseObject http.Response
+// 	json.Unmarshal(bodyBytes, &ObjectData)
+
+// 	return ObjectData
+// }
